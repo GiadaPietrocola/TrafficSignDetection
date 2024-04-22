259c5355ac76b947057b02afd56175bf16d6fc90
Achille <cannavaleachille@gmail.com>
2024-04-22 10:48:27 +0200

    main

--- myproject/main.cpp
+++ myproject/main.cpp
@@ -15,7 +15,7 @@
 int main()
 {
 
-	cv::Mat img = cv::imread(std::string(EXAMPLE_IMAGES_PATH) + "/giada4.jpeg");
+	cv::Mat img = cv::imread(std::string(EXAMPLE_IMAGES_PATH) + "/prova.jpg");
 
 	ipa::imshow("Original image", img, true, 0.5f);
 
--- myproject/utils.h
+++ myproject/utils.h
@@ -23,6 +23,29 @@ float max_area_perc = 0.5f;
 
 struct Utils
 {
+    static cv::Mat histogramEqualization(cv::Mat img)
+    {
+        std::vector<int> hist = ucas::histogram(img);
+
+        int L = 256;
+        std::vector<int> hist_eq_LUT(L);
+        float norm_factor = float(L - 1) / (img.rows * img.cols);
+        int accum = 0;
+        for (int k = 0; k < hist_eq_LUT.size(); k++)
+        {
+            accum += hist[k];
+            hist_eq_LUT[k] = norm_factor * accum;
+        }
+
+        for (int y = 0; y < img.rows; y++)
+        {
+            unsigned char *yRow = img.ptr<unsigned char>(y);
+            for (int x = 0; x < img.cols; x++)
+                yRow[x] = hist_eq_LUT[yRow[x]];
+        }
+
+        return img;
+    }
 
     // //------------------------------------------------------------------------------
     // //======================= METHODS =================================
@@ -36,19 +59,33 @@ struct Utils
     static void RealCanny(cv::Mat &img_in, cv::Mat &img_blurred, cv::Mat &edges, int sigma)
     {
 
-        cv::cvtColor(img_in, img_blurred, cv::COLOR_BGR2GRAY);
-        double canny_thresh = cv::threshold(img_blurred, img_blurred, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
-
         int kernel_size = ucas::round(6 * sigma);
         if (kernel_size % 2 == 0)
             kernel_size += 1;
 
-        cv::GaussianBlur(img_in, img_blurred, cv::Size(kernel_size, kernel_size), sigma);
+        std::vector<cv::Mat> img_channels(3);
+        cv::split(img_in, img_channels);
+        histogramEqualization(img_channels[0]);
+        histogramEqualization(img_channels[1]);
+        histogramEqualization(img_channels[2]);
+        cv::Mat img_output_BGReq;
+        cv::merge(img_channels, img_output_BGReq);
+        ipa::imshow("HE on BGR", img_output_BGReq);
+
+        ipa::imshow("Contrast", img_output_BGReq);
+
+        cv::cvtColor(img_output_BGReq, img_output_BGReq, cv::COLOR_BGR2GRAY);
+
+        cv::GaussianBlur(img_output_BGReq, img_blurred, cv::Size(kernel_size, kernel_size), sigma);
+
+        double canny_thresh = 200;
 
         cv::Canny(img_blurred, edges, canny_thresh / 3, canny_thresh, 3, false);
 
         // A Close operation could be useful??
-        cv::morphologyEx(edges, edges, cv::MORPH_DILATE,
-                         cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
+        // cv::morphologyEx(edges, edges, cv::MORPH_DILATE,
+        // cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
+
+        cv::imwrite(std::string(EXAMPLE_IMAGES_PATH) + "/edges.jpeg", edges);
     }
 };
